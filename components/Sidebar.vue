23d26e74b98e1f1598359d4a9252517389dca3c7
alfiansyahichsan <alfiansyah.ichsan@gmail.com>
2022-02-07 23:50:10 +0700

    setup tailwind postcss default layout

---
+++ components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <div class="wrapper">
+      <div class="content">Sidebar</div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+}
+</script>
+
+<style lang="postcss" scoped>
+nav {
+  @apply flex w-72 h-full bg-pink-500;
+  .wrapper {
+    @apply w-full flex mx-auto px-6 py-8;
+    .content {
+      @apply w-full h-full flex justify-center text-gray-900 text-xl;
+    }
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex h-screen bg-red-500">
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <div class="flex h-full">
+        <Sidebar />
+        <main
+          class="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14"
+        >
+          <Nuxt />
+        </main>
+        <!-- <nav class="flex w-72 h-full bg-yellow-400">
+          <div class="w-full flex mx-auto px-6 py-8">
+            <div
+              class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed"
+            >
+              Rightbar
+            </div>
+          </div>
+        </nav> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: { Sidebar },
+}
+</script>
